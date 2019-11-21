9a85795ca4efd1b52f35332906d39da5627efba2
Jenni Nemeth <j3nninemeth@gmail.com>
2019-11-20 16:02:04 -0800

    created the sign up feature with email and password, and storing user Data in database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app'; 
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -10,6 +10,33 @@ const config = {
     storageBucket: "crwn-db-9a7ac.appspot.com",
     messagingSenderId: "351288073821",
     appId: "1:351288073821:web:726fd5c9c2975acc30b7b4"
+  };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    // if sign-in fails, then exit function
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error){
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
 };
 
 firebase.initializeApp(config);
@@ -18,7 +45,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'accessToken' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+
